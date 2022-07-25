f11dd4c7b24d20c8619a06cace2b736ed54f44a0
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2022-07-25 13:49:09 -0500

    add gasLevelCode DTO validations

--- src/dto/linearity-summary.dto.ts
+++ src/dto/linearity-summary.dto.ts
@@ -1,17 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, ValidationArguments } from 'class-validator';
+import { IsNotEmpty, ValidateIf, ValidationArguments } from 'class-validator';
+import { IsValidCode } from '../pipes/is-valid-code.pipe';
+import { GasLevelCodes } from '../enums/gas-level-code.enum';
 import { IsNotNegative } from '../pipes/is-not-negative.pipe';
 import {
   LinearityInjectionImportDTO,
   LinearityInjectionDTO,
 } from './linearity-injection.dto';
+import { GasLevelCode } from '../entities/workspace/gas-level-code.entity';
+GasLevelCodes;
 
 const KEY = 'Linearity Summary';
+
+const VALID_CODES_FOR_GAS_LEVEL_CODE_VALIDATION = [
+  GasLevelCodes.HIGH,
+  GasLevelCodes.MID,
+  GasLevelCodes.LOW,
+];
 
 export class LinearitySummaryBaseDTO {
   @ApiProperty({
     description: 'gasLevelCode. ADD TO PROPERTY METADATA',
   })
+  @IsNotEmpty({
+    message: (args: ValidationArguments) => {
+      return `You did not provide [${args.property}], which is required for [${KEY}].`;
+    },
+  })
+  @IsValidCode(GasLevelCode, {
+    message: (args: ValidationArguments) => {
+      return `You reported a [${args.property}], that is not in the list of valid values.`;
+    },
+  })
+  @ValidateIf(o =>
+    VALID_CODES_FOR_GAS_LEVEL_CODE_VALIDATION.includes(o.gasLevelCode),
+  )
   gasLevelCode: string;
 
   @ApiProperty({
