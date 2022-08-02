f357eb544369385a11fe59ef9edc905018f1f40a
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-08-02 15:23:49 -0400

    Resolve high critical error

--- src/interceptors/format-validation-errors.interceptor.ts
+++ src/interceptors/format-validation-errors.interceptor.ts
@@ -12,7 +12,7 @@ export class FormatValidationErrorsInterceptor implements NestInterceptor {
   intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(err => {
-        const regex = /((test|linearity|cycleTime|appECorrelationTest|Hg)Summary|certificationEvent|testExtensionExemption|calibrationInjection|rata|flowToLoad(Reference|Check)|onlineOfflineCalibration|fuelFlowmeterAccuracy|transmitterTransducer|fuelFlowToLoad(Baseline|Test)|testQualification|protocolGas|(unitDefault|airEmission)Test)Data.\d./;
+        //const regex = /((test|linearity|cycleTime|appECorrelationTest|Hg)Summary|certificationEvent|testExtensionExemption|calibrationInjection|rata|flowToLoad(Reference|Check)|onlineOfflineCalibration|fuelFlowmeterAccuracy|transmitterTransducer|fuelFlowToLoad(Baseline|Test)|testQualification|protocolGas|(unitDefault|airEmission)Test)Data.\d./;
         //const regex = /(testSummary|certificationEvent|testExtensionExemption|calibrationInjection|linearitySummary|rata|flowToLoadReference|flowToLoadCheck|cycleTimeSummary|onlineOfflineCalibration|fuelFlowmeterAccuracy|transmitterTransducer|fuelFlowToLoadBaseline|fuelFlowToLoadTest|appECorrelationTestSummary|unitDefaultTest|hgSummary|testQualification|protocolGas|airEmissionTest)Data.\d./;
         console.log(err);
         if (
@@ -23,7 +23,10 @@ export class FormatValidationErrorsInterceptor implements NestInterceptor {
           err.response.message.length > 0
         ) {
           err.response.message = err.response.message.map((e: string) =>
-            e.replace(regex, ''),
+            e.replace(
+              /((test|linearity|cycleTime|appECorrelationTest|Hg)Summary|certificationEvent|testExtensionExemption|calibrationInjection|rata|flowToLoad(Reference|Check)|onlineOfflineCalibration|fuelFlowmeterAccuracy|transmitterTransducer|fuelFlowToLoad(Baseline|Test)|testQualification|protocolGas|(unitDefault|airEmission)Test)Data.\d./,
+              '',
+            ),
           );
         }
         return throwError(() => err);
