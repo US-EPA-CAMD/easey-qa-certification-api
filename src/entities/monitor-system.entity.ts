38e87f902586427a68dc8746f0970c2253ff4a6a
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2022-08-11 16:35:17 -0500

    add systemTypeCode to monitor-system entity

--- src/entities/monitor-system.entity.ts
+++ src/entities/monitor-system.entity.ts
@@ -27,6 +27,12 @@ export class MonitorSystem extends BaseEntity {
   })
   monitoringSystemID: string;
 
+  @Column({
+    type: 'varchar',
+    name: 'sys_type_cd',
+  })
+  systemTypeCode: string;
+
   @ManyToOne(
     () => MonitorLocation,
     ml => ml.systems,
