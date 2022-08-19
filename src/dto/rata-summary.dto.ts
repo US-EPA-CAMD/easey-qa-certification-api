46cfd109aba603f54a6116013e58a6c58cd1a9ef
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2022-08-18 23:14:57 -0500

    add api property to base dto

--- src/dto/rata-summary.dto.ts
+++ src/dto/rata-summary.dto.ts
@@ -1,24 +1,100 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { RataRunImportDTO } from './rata-run.dto';
 
 export class RataSummaryBaseDTO {
+  @ApiProperty({
+    description: 'relativeAccuracy. ADD TO PROPERTY METADATA',
+  })
   relativeAccuracy: number;
+
+  @ApiProperty({
+    description: 'biasAdjustmentFactor. ADD TO PROPERTY METADATA',
+  })
   biasAdjustmentFactor: number;
+
+  @ApiProperty({
+    description: 'meanCEMValue. ADD TO PROPERTY METADATA',
+  })
   meanCEMValue: number;
+
+  @ApiProperty({
+    description: 'meanRataReferenceValue. ADD TO PROPERTY METADATA',
+  })
   meanRataReferenceValue: number;
+
+  @ApiProperty({
+    description: 'operatingLevelCode. ADD TO PROPERTY METADATA',
+  })
   operatingLevelCode: string;
+
+  @ApiProperty({
+    description: 'meanDifference. ADD TO PROPERTY METADATA',
+  })
   meanDifference: number;
+
+  @ApiProperty({
+    description: 'defaultWaf. ADD TO PROPERTY METADATA',
+  })
   defaultWaf: number;
+
+  @ApiProperty({
+    description: 'averageGrossUnitLoad. ADD TO PROPERTY METADATA',
+  })
   averageGrossUnitLoad: number;
+
+  @ApiProperty({
+    description: 'apsIndicator. ADD TO PROPERTY METADATA',
+  })
   apsIndicator: number;
+
+  @ApiProperty({
+    description: 'standardDeviationDifference. ADD TO PROPERTY METADATA',
+  })
   standardDeviationDifference: number;
+
+  @ApiProperty({
+    description: 'confidenceCoefficent. ADD TO PROPERTY METADATA',
+  })
   confidenceCoefficent: number;
+
+  @ApiProperty({
+    description: 'co2OrO2ReferenceMethodCode. ADD TO PROPERTY METADATA',
+  })
   co2OrO2ReferenceMethodCode: string;
+
+  @ApiProperty({
+    description: 'referenceMethodCode. ADD TO PROPERTY METADATA',
+  })
   referenceMethodCode: string;
+
+  @ApiProperty({
+    description: 'tValue. ADD TO PROPERTY METADATA',
+  })
   tValue: number;
+
+  @ApiProperty({
+    description: 'stackDiameter. ADD TO PROPERTY METADATA',
+  })
   stackDiameter: number;
+
+  @ApiProperty({
+    description: 'stackArea. ADD TO PROPERTY METADATA',
+  })
   stackArea: number;
+
+  @ApiProperty({
+    description: 'calculatedWaf. ADD TO PROPERTY METADATA',
+  })
   calculatedWaf: number;
+
+  @ApiProperty({
+    description: 'numberOfTraversePoints. ADD TO PROPERTY METADATA',
+  })
   numberOfTraversePoints: number;
+
+  @ApiProperty({
+    description: 'apsCode. ADD TO PROPERTY METADATA',
+  })
   apsCode: string;
 }
 
@@ -42,6 +118,9 @@ export class RataSummaryRecordDTO extends RataSummaryBaseDTO {
   updateDate: string;
 }
 
-export class RataSummaryImporetData extends RataSummaryBaseDTO {
+export class RataSummaryImportDTO extends RataSummaryBaseDTO {
+  rataRunData: RataRunImportDTO[];
+}
+export class RataSummaryDTO extends RataSummaryBaseDTO {
   rataRunData: RataRunImportDTO[];
 }
