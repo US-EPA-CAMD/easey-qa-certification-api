d06acd22b56655a56550ab5a27acea7fae00b5e1
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2022-09-21 07:38:00 +0000

    add air emission test global view endpoints

--- src/air-emission-testing/air-emission-testing.service.ts
+++ src/air-emission-testing/air-emission-testing.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LoggingException } from '@us-epa-camd/easey-common/exceptions';
+import { AirEmissionTestingDTO } from '../dto/air-emission-test.dto';
+import { AirEmissionTestingMap } from '../maps/air-emission-testing.map';
+import { AirEmissionTestingRepository } from './air-emission-testing.repository';
 
 @Injectable()
-export class AirEmissionTestingService {}
+export class AirEmissionTestingService {
+  constructor(
+    @InjectRepository(AirEmissionTestingRepository)
+    private readonly repository: AirEmissionTestingRepository,
+    private readonly map: AirEmissionTestingMap,
+  ) {}
+
+  async getAirEmissionsTestings(
+    testSumId: string,
+  ): Promise<AirEmissionTestingDTO[]> {
+    const records = await this.repository.find({ where: { testSumId } });
+
+    return this.map.many(records);
+  }
+
+  async getAirEmissionsTesting(id: string): Promise<AirEmissionTestingDTO> {
+    const result = await this.repository.findOne(id);
+
+    if (!result) {
+      throw new LoggingException(
+        `Air Emissions Testing record not found with Record Id [${id}].`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return this.map.one(result);
+  }
+}
