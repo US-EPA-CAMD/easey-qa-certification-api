90f74355b18769a93702d51d667ebbd6cc8b179e
subrataroy1 <subrataroy@cvpcorp.com>
2022-10-03 09:32:13 -0400

    chore: code smells

--- src/app-e-correlation-test-summary/app-e-correlation-test-summary.module.ts
+++ src/app-e-correlation-test-summary/app-e-correlation-test-summary.module.ts
@@ -1,9 +1,7 @@
-import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppECorrelationTestSummaryMap } from 'src/maps/app-e-correlation-summary.map';
 import { AppECorrelationTestSummaryService } from './app-e-correlation-test-summary.service';
 import { AppendixETestSummaryController } from './app-e-correlation-test-summary.controller';
-import { TestSummaryModule } from 'src/test-summary/test-summary.module';
 import { AppendixETestSummaryRepository } from './app-e-correlation-test-summary.repository';
 import { AppECorrelationTestRunModule } from '../app-e-correlation-test-run/app-e-correlation-test-run.module';
 
