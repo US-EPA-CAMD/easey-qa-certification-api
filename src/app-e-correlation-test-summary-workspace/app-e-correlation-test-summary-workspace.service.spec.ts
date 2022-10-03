c62b37d57638e0bbfe723fa0aaa247e6a727b3ad
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2022-10-03 14:30:14 -0400

    unit tests for service

--- src/app-e-correlation-test-summary-workspace/app-e-correlation-test-summary-workspace.service.spec.ts
+++ src/app-e-correlation-test-summary-workspace/app-e-correlation-test-summary-workspace.service.spec.ts
@@ -1,20 +1,83 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  AppECorrelationTestSummaryBaseDTO,
+  AppECorrelationTestSummaryDTO,
+} from '../dto/app-e-correlation-test-summary.dto';
+import { AppECorrelationTestSummary } from '../entities/app-e-correlation-test-summary.entity';
+import { AppECorrelationTestSummaryMap } from '../maps/app-e-correlation-summary.map';
+import { TestSummaryWorkspaceService } from '../test-summary-workspace/test-summary.service';
+import { AppendixETestSummaryWorkspaceRepository } from './app-e-correlation-test-summary-workspace.repository';
 import { AppECorrelationTestSummaryWorkspaceService } from './app-e-correlation-test-summary-workspace.service';
+
+const testSumId = '';
+const userId = 'user';
+const entity = new AppECorrelationTestSummary();
+const appECorrelationTest = new AppECorrelationTestSummaryDTO();
+const appECorrelationTests = [appECorrelationTest];
+
+const payload = new AppECorrelationTestSummaryBaseDTO();
+
+const mockRepository = () => ({
+  find: jest.fn().mockResolvedValue([entity]),
+  findOne: jest.fn().mockResolvedValue(entity),
+  save: jest.fn().mockResolvedValue(entity),
+  create: jest.fn().mockResolvedValue(entity),
+});
+
+const mockMap = () => ({
+  one: jest.fn().mockResolvedValue(appECorrelationTest),
+  many: jest.fn().mockResolvedValue(appECorrelationTests),
+});
+
+const mockTestSumService = () => ({
+  resetToNeedsEvaluation: jest.fn(),
+});
 
 describe('AppECorrelationTestSummaryWorkspaceService', () => {
   let service: AppECorrelationTestSummaryWorkspaceService;
+  let testSummaryService: TestSummaryWorkspaceService;
+  let repository: AppendixETestSummaryWorkspaceRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AppECorrelationTestSummaryWorkspaceService],
+      providers: [
+        AppECorrelationTestSummaryWorkspaceService,
+        {
+          provide: TestSummaryWorkspaceService,
+          useFactory: mockTestSumService,
+        },
+        {
+          provide: AppendixETestSummaryWorkspaceRepository,
+          useFactory: mockRepository,
+        },
+        {
+          provide: AppECorrelationTestSummaryMap,
+          useFactory: mockMap,
+        },
+      ],
     }).compile();
 
     service = module.get<AppECorrelationTestSummaryWorkspaceService>(
       AppECorrelationTestSummaryWorkspaceService,
     );
+    testSummaryService = module.get<TestSummaryWorkspaceService>(
+      TestSummaryWorkspaceService,
+    );
+    repository = module.get<AppendixETestSummaryWorkspaceRepository>(
+      AppendixETestSummaryWorkspaceRepository,
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createAppECorrelation', () => {
+    it('Calls the service to create a new Appendix E Correlation Test Summary record', async () => {
+      const result = await service.createAppECorrelation(
+        testSumId,
+        payload,
+        userId,
+      );
+
+      expect(result).toEqual(appECorrelationTest);
+      expect(testSummaryService.resetToNeedsEvaluation).toHaveBeenCalled();
+    });
   });
 });
--- src/app-e-correlation-test-summary/app-e-correlation-test-summary.service.spec.ts
+++ src/app-e-correlation-test-summary/app-e-correlation-test-summary.service.spec.ts
@@ -1,20 +1,78 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  AppECorrelationTestSummaryBaseDTO,
+  AppECorrelationTestSummaryDTO,
+} from '../dto/app-e-correlation-test-summary.dto';
+import { AppECorrelationTestSummary } from '../entities/app-e-correlation-test-summary.entity';
+import { AppECorrelationTestSummaryMap } from '../maps/app-e-correlation-summary.map';
+import { TestSummaryWorkspaceService } from '../test-summary-workspace/test-summary.service';
+import { AppendixETestSummaryRepository } from './app-e-correlation-test-summary.repository';
 import { AppECorrelationTestSummaryService } from './app-e-correlation-test-summary.service';
 
-describe('AppECorrelationTestSummaryService', () => {
+const testSumId = '';
+
+const entity = new AppECorrelationTestSummary();
+const appECorrelationTest = new AppECorrelationTestSummaryDTO();
+const appECorrelationTests = [appECorrelationTest];
+
+const payload = new AppECorrelationTestSummaryBaseDTO();
+
+const mockRepository = () => ({
+  find: jest.fn().mockResolvedValue([entity]),
+  findOne: jest.fn().mockResolvedValue(entity),
+  save: jest.fn().mockResolvedValue(entity),
+  create: jest.fn().mockResolvedValue(entity),
+});
+
+const mockMap = () => ({
+  one: jest.fn().mockResolvedValue(appECorrelationTest),
+  many: jest.fn().mockResolvedValue(appECorrelationTests),
+});
+
+const mockTestSumService = () => ({
+  resetToNeedsEvaluation: jest.fn(),
+});
+
+describe('AppECorrelationTestSummaryWorkspaceService', () => {
   let service: AppECorrelationTestSummaryService;
+  let testSummaryService: TestSummaryWorkspaceService;
+  let repository: AppendixETestSummaryRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AppECorrelationTestSummaryService],
+      providers: [
+        AppECorrelationTestSummaryService,
+        {
+          provide: TestSummaryWorkspaceService,
+          useFactory: mockTestSumService,
+        },
+        {
+          provide: AppendixETestSummaryRepository,
+          useFactory: mockRepository,
+        },
+        {
+          provide: AppECorrelationTestSummaryMap,
+          useFactory: mockMap,
+        },
+      ],
     }).compile();
 
     service = module.get<AppECorrelationTestSummaryService>(
       AppECorrelationTestSummaryService,
     );
+    testSummaryService = module.get<TestSummaryWorkspaceService>(
+      TestSummaryWorkspaceService,
+    );
+    repository = module.get<AppendixETestSummaryRepository>(
+      AppendixETestSummaryRepository,
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createAppECorrelation', () => {
+    it('Calls the service to create a new Appendix E Correlation Test Summary record', async () => {
+      const result = await service.getAppECorrelation(testSumId);
+      expect(result).toEqual(appECorrelationTest);
+      expect(testSummaryService.resetToNeedsEvaluation).toHaveBeenCalled();
+    });
   });
 });
