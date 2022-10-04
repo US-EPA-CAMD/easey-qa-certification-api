c403c5b8af9355c21f794de097f955a2aa91fbc8
Jason Whitehead <jwhitehead@austin.rr.com>
2022-10-04 16:51:50 -0500

    removed dep on test sum in appe not needed

--- src/app-e-correlation-test-summary/app-e-correlation-test-summary.service.ts
+++ src/app-e-correlation-test-summary/app-e-correlation-test-summary.service.ts
@@ -1,17 +1,14 @@
-import { forwardRef, Inject, Injectable, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AppendixETestSummaryRepository } from './app-e-correlation-test-summary.repository';
 import { AppECorrelationTestSummaryMap } from '../maps/app-e-correlation-summary.map';
 import { AppECorrelationTestSummaryRecordDTO } from '../dto/app-e-correlation-test-summary.dto';
 import { LoggingException } from '@us-epa-camd/easey-common/exceptions';
-import { TestSummaryService } from '../test-summary/test-summary.service';
 
 @Injectable()
 export class AppECorrelationTestSummaryService {
   constructor(
     private readonly map: AppECorrelationTestSummaryMap,
-    @Inject(forwardRef(() => TestSummaryService))
-    private readonly testSummaryService: TestSummaryService,
     @InjectRepository(AppendixETestSummaryRepository)
     private readonly repository: AppendixETestSummaryRepository,
   ) {}
