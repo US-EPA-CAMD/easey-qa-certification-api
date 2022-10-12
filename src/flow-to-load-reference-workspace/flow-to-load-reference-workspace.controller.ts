7af1445bf1e3aaec43ccacf6b3ef82c8135edfc8
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2022-10-12 11:02:21 -0400

    added flow to load reference workspace folder with controller

---
+++ src/flow-to-load-reference-workspace/flow-to-load-reference-workspace.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Param, Post, Body, UseGuards, Get } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
+import { User } from '@us-epa-camd/easey-common/decorators';
+import { AuthGuard } from '@us-epa-camd/easey-common/guards';
+import { CurrentUser } from '@us-epa-camd/easey-common/interfaces';
+
+import {
+  FlowToLoadReferenceBaseDTO,
+  FlowToLoadReferenceRecordDTO,
+} from '../dto/flow-to-load-reference.dto';
+
+@Controller()
+@ApiSecurity('APIKey')
+@ApiTags('Flow To Load Reference')
+export class FlowToLoadReferenceWorkspaceController {
+  constructor(private readonly service: FlowToLoadCheckWorkspaceService) {}
+
+  @Post()
+  @ApiBearerAuth('Token')
+  @UseGuards(AuthGuard)
+  @ApiCreatedResponse({
+    type: FlowToLoadReferenceRecordDTO,
+    description: 'Creates a workspace Flow To Load Reference record.',
+  })
+  async createFlowToLoadReference(
+    @Param('locId') _locationId: string,
+    @Param('testSumId') testSumId: string,
+    @Body() payload: FlowToLoadReferenceBaseDTO,
+    @User() user: CurrentUser,
+  ): Promise<FlowToLoadReferenceRecordDTO> {
+    return this.service.createFlowToLoadReference(
+      testSumId,
+      payload,
+      user.userId,
+    );
+  }
+}
