43794fe0bf571fc049cd932d49faf0fe9d4492d6
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2022-10-13 09:19:53 -0400

    created view endpoints and unit tests

--- src/flow-to-load-reference-workspace/flow-to-load-reference-workspace.controller.ts
+++ src/flow-to-load-reference-workspace/flow-to-load-reference-workspace.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Param, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Param, Post, Body, UseGuards, Get } from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiCreatedResponse,
+  ApiOkResponse,
   ApiSecurity,
   ApiTags,
 } from '@nestjs/swagger';
@@ -20,6 +21,35 @@ import { FlowToLoadReferenceWorkspaceService } from './flow-to-load-reference-wo
 @ApiTags('Flow To Load Reference')
 export class FlowToLoadReferenceWorkspaceController {
   constructor(private readonly service: FlowToLoadReferenceWorkspaceService) {}
+
+  @Get()
+  @ApiOkResponse({
+    isArray: true,
+    type: FlowToLoadReferenceRecordDTO,
+    description:
+      'Retrieves workspace Flow To Load Reference records by Test Summary Id',
+  })
+  async getFlowToLoadReferences(
+    @Param('locId') _locationId: string,
+    @Param('testSumId') testSumId: string,
+  ): Promise<FlowToLoadReferenceRecordDTO[]> {
+    return this.service.getFlowToLoadReferences(testSumId);
+  }
+
+  @Get(':id')
+  @ApiOkResponse({
+    isArray: false,
+    type: FlowToLoadReferenceRecordDTO,
+    description:
+      'Retrieves a workspace Flow To Load Reference record by its Id',
+  })
+  async getFlowToLoadReference(
+    @Param('locId') _locationId: string,
+    @Param('testSumId') _testSumId: string,
+    @Param('id') id: string,
+  ): Promise<FlowToLoadReferenceRecordDTO> {
+    return this.service.getFlowToLoadReference(id);
+  }
 
   @Post()
   @ApiBearerAuth('Token')
