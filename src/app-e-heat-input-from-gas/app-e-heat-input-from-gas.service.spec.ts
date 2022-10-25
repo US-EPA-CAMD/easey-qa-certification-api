b9f33e87ea895597f25ce9e44575575f47a51741
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2022-10-25 09:41:24 -0500

    add unit tests for app-e-heat-input-from-gas

--- src/app-e-heat-input-from-gas/app-e-heat-input-from-gas.service.spec.ts
+++ src/app-e-heat-input-from-gas/app-e-heat-input-from-gas.service.spec.ts
@@ -1,33 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Logger } from '@us-epa-camd/easey-common/logger';
 import { TestSummaryService } from '../test-summary/test-summary.service';
-import { AppEHeatInputFromGasDTO } from '../dto/app-e-heat-input-from-gas.dto';
+import { AppEHeatInputFromGasRecordDTO } from '../dto/app-e-heat-input-from-gas.dto';
 import { AppEHeatInputFromGas } from '../entities/app-e-heat-input-from-gas.entity';
 import { AppEHeatInputFromGasMap } from '../maps/app-e-heat-input-from-gas.map';
 import { AppEHeatInputFromGasRepository } from './app-e-heat-input-from-gas.repository';
 import { AppEHeatInputFromGasService } from './app-e-heat-input-from-gas.service';
 
 const appECorrTestRunId = 'd4e6f7';
-const mockAeHiFromGas = new AppEHeatInputFromGas();
-const mockAeHiFromGasDTO = new AppEHeatInputFromGasDTO();
+const appEHIGasId = 'a1b2c3';
+const appEHIGasRecord = new AppEHeatInputFromGasRecordDTO();
+const appEHIGasRecords = [appEHIGasRecord];
 
 const mockTestSumService = () => ({
   resetToNeedsEvaluation: jest.fn(),
 });
 
 const mockRepository = () => ({
-  find: jest.fn().mockResolvedValue([mockAeHiFromGas]),
-  create: jest.fn().mockResolvedValue(mockAeHiFromGas),
-  save: jest.fn().mockResolvedValue(mockAeHiFromGas),
-  findOne: jest.fn().mockResolvedValue(mockAeHiFromGas),
+  find: jest.fn().mockResolvedValue(appEHIGasRecords),
+  create: jest.fn().mockResolvedValue(appEHIGasRecord),
+  save: jest.fn().mockResolvedValue(appEHIGasRecord),
+  findOne: jest.fn().mockResolvedValue(appEHIGasRecord),
+  getAppEHeatInputFromGasById: jest.fn().mockResolvedValue(appEHIGasRecord),
+  getAppEHeatInputFromGasByTestRunId: jest
+    .fn()
+    .mockResolvedValue(appEHIGasRecords),
 });
 
 const mockMap = () => ({
-  one: jest.fn().mockResolvedValue(mockAeHiFromGasDTO),
-  many: jest.fn().mockResolvedValue([mockAeHiFromGasDTO]),
+  one: jest.fn().mockResolvedValue(appEHIGasRecord),
+  many: jest.fn().mockResolvedValue(appEHIGasRecords),
 });
 
-describe('AppEHeatInputFromGasWorkspaceService', () => {
+describe('AppEHeatInputFromGasService', () => {
   let service: AppEHeatInputFromGasService;
   let repository: AppEHeatInputFromGasRepository;
 
@@ -54,18 +59,45 @@ describe('AppEHeatInputFromGasWorkspaceService', () => {
     service = module.get<AppEHeatInputFromGasService>(
       AppEHeatInputFromGasService,
     );
+    repository = module.get<AppEHeatInputFromGasRepository>(
+      AppEHeatInputFromGasRepository,
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getAppEHeatInputFromGases', () => {
+    it('calls the repository.find() to get Appendix E Heat Input from Gas records by Appendix E Correlation Test Run Id', async () => {
+      const result = await service.getAppEHeatInputFromGases(appECorrTestRunId);
+      expect(result).toEqual(appEHIGasRecords);
+    });
+  });
+
+  describe('getAppEHeatInputFromGas', () => {
+    it('Calls repository.findOne({id}) to get a Appendix E Heat Input from Oil record', async () => {
+      const result = await service.getAppEHeatInputFromGas(appEHIGasId);
+      expect(result).toEqual(appEHIGasRecord);
+    });
+
+    it('Should throw error when record not found', async () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
+
+      let errored = false;
+
+      try {
+        await service.getAppEHeatInputFromGas('kdiene');
+      } catch (err) {
+        errored = true;
+      }
+
+      expect(errored).toBe(true);
+    });
   });
 
   describe('getAppEHeatInputFromGasByTestRunIds', () => {
-    it('Should get Appendix E Heat Input From Gas records by test sum ids', async () => {
+    it('Should get Appendix E Heat Input From Oil records by test sum ids', async () => {
       const result = await service.getAppEHeatInputFromGasByTestRunIds([
         appECorrTestRunId,
       ]);
-      expect(result).toEqual([mockAeHiFromGas]);
+      expect(result).toEqual(appEHIGasRecords);
     });
   });
 
