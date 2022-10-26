5858dddbd4efd5109600f30556ea6db66f2d16ee
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2022-10-26 10:12:59 -0400

    increasing coverage for linearity workspace

--- src/linearity-summary-workspace/linearity-summary.service.spec.ts
+++ src/linearity-summary-workspace/linearity-summary.service.spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { InternalServerErrorException } from '@nestjs/common';
 import { LoggerModule } from '@us-epa-camd/easey-common/logger';
 import { LinearitySummary } from '../entities/linearity-summary.entity';
 import { LinearitySummaryRepository } from '../linearity-summary/linearity-summary.repository';
-import { LinearityInjectionImportDTO } from '../dto/linearity-injection.dto';
 import {
+  LinearitySummaryBaseDTO,
   LinearitySummaryDTO,
   LinearitySummaryImportDTO,
-  LinearitySummaryRecordDTO,
 } from '../dto/linearity-summary.dto';
 import { LinearityInjectionWorkspaceService } from '../linearity-injection-workspace/linearity-injection.service';
 import { LinearitySummaryMap } from '../maps/linearity-summary.map';
@@ -14,33 +14,52 @@ import { TestSummaryWorkspaceService } from '../test-summary-workspace/test-summ
 import { LinearitySummaryWorkspaceRepository } from './linearity-summary.repository';
 import { LinearitySummaryWorkspaceService } from './linearity-summary.service';
 
+const id = '';
 const testSumId = '1';
 const userId = 'testuser';
+const entity = new LinearitySummary();
+const linearitySummaryRecord = new LinearitySummaryDTO();
+const linearitySummaryRecords = [linearitySummaryRecord];
 
-const lineSummaryDto = new LinearitySummaryDTO();
-const lineSummaryRecordDto = new LinearitySummaryRecordDTO();
+const payload = new LinearitySummaryBaseDTO();
 
-const payload = new LinearitySummaryImportDTO();
-payload.linearityInjectionData = [new LinearityInjectionImportDTO()];
-
-const mockRepository = () => ({});
-const mockOfficialRepository = () => ({
-  findOne: jest.fn().mockResolvedValue(new LinearitySummary()),
+const mockRepository = () => ({
+  find: jest.fn().mockResolvedValue([entity]),
+  findOne: jest.fn().mockResolvedValue(entity),
+  save: jest.fn().mockResolvedValue(entity),
+  create: jest.fn().mockResolvedValue(entity),
+  delete: jest.fn().mockResolvedValue(null),
 });
 
-const mockTestSummaryService = () => ({});
+const mockTestSumService = () => ({
+  resetToNeedsEvaluation: jest.fn(),
+});
+
+const mockService = () => ({
+  getSummariesByTestSumId: jest.fn().mockResolvedValue(linearitySummaryRecords),
+  getSummaryById: jest.fn().mockResolvedValue(linearitySummaryRecord),
+  createSummary: jest.fn().mockResolvedValue(linearitySummaryRecord),
+  deleteSummary: jest.fn().mockResolvedValue(undefined),
+  updateSummary: jest.fn().mockResolvedValue(linearitySummaryRecord),
+});
 
 const mockLinearityInjectionService = () => ({
   import: jest.fn().mockResolvedValue(null),
 });
 
 const mockMap = () => ({
-  one: jest.fn().mockResolvedValue(lineSummaryDto),
-  many: jest.fn().mockResolvedValue([lineSummaryDto]),
+  one: jest.fn().mockResolvedValue(linearitySummaryRecord),
+  many: jest.fn().mockResolvedValue([linearitySummaryRecords]),
+});
+
+const mockOfficialRepository = () => ({
+  findOne: jest.fn(),
 });
 
 describe('LinearitySummaryWorkspaceService', () => {
   let service: LinearitySummaryWorkspaceService;
+  let testSummaryService: TestSummaryWorkspaceService;
+  let repository: LinearitySummaryWorkspaceRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -49,11 +68,15 @@ describe('LinearitySummaryWorkspaceService', () => {
         LinearitySummaryWorkspaceService,
         {
           provide: TestSummaryWorkspaceService,
-          useFactory: mockTestSummaryService,
+          useFactory: mockTestSumService,
         },
         {
           provide: LinearityInjectionWorkspaceService,
           useFactory: mockLinearityInjectionService,
+        },
+        {
+          provide: LinearitySummaryWorkspaceService,
+          useFactory: mockService,
         },
         {
           provide: LinearitySummaryWorkspaceRepository,
@@ -70,16 +93,71 @@ describe('LinearitySummaryWorkspaceService', () => {
       ],
     }).compile();
 
-    service = module.get(LinearitySummaryWorkspaceService);
+    service = module.get<LinearitySummaryWorkspaceService>(
+      LinearitySummaryWorkspaceService,
+    );
+    testSummaryService = module.get<TestSummaryWorkspaceService>(
+      TestSummaryWorkspaceService,
+    );
+    repository = module.get<LinearitySummaryWorkspaceRepository>(
+      LinearitySummaryWorkspaceRepository,
+    );
   });
 
   describe('import', () => {
     it('Should import Linearity Summary', async () => {
       jest
         .spyOn(service, 'createSummary')
-        .mockResolvedValue(lineSummaryRecordDto);
-      const result = await service.import(testSumId, payload, userId);
+        .mockResolvedValue(linearitySummaryRecord);
+      const result = await service.import(
+        testSumId,
+        new LinearitySummaryImportDTO(),
+        userId,
+      );
       expect(result).toEqual(null);
+    });
+  });
+
+  describe('getSummariesByTestSumId', () => {
+    it('Should return Linearity Summary records by Test Summary id', async () => {
+      const result = await service.getSummariesByTestSumId(testSumId);
+      expect(result).toEqual(linearitySummaryRecords);
+    });
+  });
+
+  describe('getSummaryById', () => {
+    it('Should return a Linearity Summary record', async () => {
+      const result = await service.getSummaryById(id);
+      expect(result).toEqual(linearitySummaryRecord);
+    });
+  });
+
+  describe('createSummary', () => {
+    it('Should create and return a new Linearity Summary record', async () => {
+      const result = await service.createSummary(testSumId, payload, userId);
+
+      expect(result).toEqual(linearitySummaryRecord);
+      expect(testSummaryService.resetToNeedsEvaluation).toHaveBeenCalled();
+    });
+  });
+
+  describe('editFuelFlowToLoadTest', () => {
+    it('Should update and return a new Linearity Summary record', async () => {
+      const result = await service.updateSummary(
+        testSumId,
+        id,
+        payload,
+        userId,
+      );
+      expect(result).toEqual(linearitySummaryRecord);
+    });
+  });
+
+  describe('deleteSummary', () => {
+    it('Should delete a Linearity Summary record', async () => {
+      const result = await service.deleteSummary(testSumId, id, userId);
+
+      expect(result).toEqual(undefined);
     });
   });
 });
