6c1e51ee008151003f8fced5fa43e98f8a1ae5a8
subrataroy1 <subrataroy@cvpcorp.com>
2022-10-28 13:59:34 -0400

    chore: update easey common and update flow rata run checks

--- src/dto/flow-rata-run.dto.ts
+++ src/dto/flow-rata-run.dto.ts
@@ -13,9 +13,9 @@ const MIN_STATIC_STACK_PRESSURE = -30;
 const MAX_STATIC_STACK_PRESSURE = 30;
 const MIN_BAROMETRIC_PRESSURE = 20;
 const MAX_BAROMETRIC_PRESSURE = 35;
-const MIN_PERCENT_CO2_PRESSURE = 0.1;
+const MIN_PERCENT_CO2_PRESSURE = 0;
 const MAX_PERCENT_CO2_PRESSURE = 20;
-const MIN_PERCENT_O2_PRESSURE = 0.1;
+const MIN_PERCENT_O2_PRESSURE = 0;
 const MAX_PERCENT_O2_PRESSURE = 22;
 
 export class FlowRataRunBaseDTO {
@@ -71,17 +71,23 @@ export class FlowRataRunBaseDTO {
       });
     },
   })
-  @IsInRange(0.1, MAX_PERCENT_CO2_PRESSURE, {
-    message: (args: ValidationArguments) => {
-      return CheckCatalogService.formatResultMessage('RATA-65-B', {
-        value: args.value,
-        fieldname: args.property,
-        key: KEY,
-        minvalue: MIN_PERCENT_CO2_PRESSURE,
-        maxvalue: MAX_PERCENT_CO2_PRESSURE,
-      });
+  @IsInRange(
+    MIN_PERCENT_O2_PRESSURE,
+    MAX_PERCENT_CO2_PRESSURE,
+    {
+      message: (args: ValidationArguments) => {
+        return CheckCatalogService.formatResultMessage('RATA-65-B', {
+          value: args.value,
+          fieldname: args.property,
+          key: KEY,
+          minvalue: MIN_PERCENT_CO2_PRESSURE,
+          maxvalue: MAX_PERCENT_CO2_PRESSURE,
+        });
+      },
     },
-  })
+    false,
+    false,
+  )
   percentCO2: number;
 
   @IsNotEmpty({
@@ -92,17 +98,23 @@ export class FlowRataRunBaseDTO {
       });
     },
   })
-  @IsInRange(0.1, MAX_PERCENT_O2_PRESSURE, {
-    message: (args: ValidationArguments) => {
-      return CheckCatalogService.formatResultMessage('RATA-66-B', {
-        value: args.value,
-        fieldname: args.property,
-        key: KEY,
-        minvalue: MIN_PERCENT_O2_PRESSURE,
-        maxvalue: MAX_PERCENT_O2_PRESSURE,
-      });
+  @IsInRange(
+    MIN_PERCENT_O2_PRESSURE,
+    MAX_PERCENT_O2_PRESSURE,
+    {
+      message: (args: ValidationArguments) => {
+        return CheckCatalogService.formatResultMessage('RATA-66-B', {
+          value: args.value,
+          fieldname: args.property,
+          key: KEY,
+          minvalue: MIN_PERCENT_O2_PRESSURE,
+          maxvalue: MAX_PERCENT_O2_PRESSURE,
+        });
+      },
     },
-  })
+    false,
+    false,
+  )
   percentO2: number;
 
   percentMoisture: number;
