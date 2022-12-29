ca72a48849eeca9b9663f1683c1b8db9b384ed91
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2022-12-29 01:30:59 -0600

    correct db name in hg injection entities

--- src/hg-summary/hg-summary.module.ts
+++ src/hg-summary/hg-summary.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { HgSummaryService } from './hg-summary.service';
 import { HgSummaryController } from './hg-summary.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { HgSummaryMap } from '../maps/hg-summary.map';
 import { HgSummaryRepository } from './hg-summary.repository';
+import { HgInjectionModule } from '../hg-injection/hg-injection.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HgSummaryRepository])],
+  imports: [
+    TypeOrmModule.forFeature([HgSummaryRepository]),
+    forwardRef(() => HgInjectionModule),
+  ],
   controllers: [HgSummaryController],
   providers: [HgSummaryMap, HgSummaryService],
   exports: [TypeOrmModule, HgSummaryMap, HgSummaryService],
