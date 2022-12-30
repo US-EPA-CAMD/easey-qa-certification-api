e21310dc0ef5680ee0dd9f23abae69ce3b3fe542
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2022-12-30 10:54:08 -0500

    updating the import path of HgInjection workspace entity

--- src/hg-injection-workspace/hg-injection-workspace.repository.ts
+++ src/hg-injection-workspace/hg-injection-workspace.repository.ts
@@ -1,4 +1,4 @@
-import { HgInjection } from '../entities/hg-injection.entity';
+import { HgInjection } from '../entities/workspace/hg-injection.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(HgInjection)
