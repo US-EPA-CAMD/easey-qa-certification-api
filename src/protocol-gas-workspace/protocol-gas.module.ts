0ce189f2f9405236cb5e8958a7b7e54054c6feea
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2023-01-30 10:13:02 -0500

    fixing bug

--- src/protocol-gas-workspace/protocol-gas.module.ts
+++ src/protocol-gas-workspace/protocol-gas.module.ts
@@ -9,12 +9,14 @@ import { ProtocolGasWorkspaceController } from './protocol-gas.controller';
 import { ProtocolGasWorkspaceRepository } from './protocol-gas.repository';
 import { ProtocolGasMap } from '../maps/protocol-gas.map';
 import { ProtocolGasChecksService } from './protocol-gas-checks.service';
+import { GasComponentCodeModule } from '../gas-component-code/gas-component-code.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProtocolGasWorkspaceRepository]),
     forwardRef(() => TestSummaryWorkspaceModule),
     forwardRef(() => ProtocolGasModule),
+    forwardRef(() => GasComponentCodeModule),
     HttpModule,
   ],
   controllers: [ProtocolGasWorkspaceController],
