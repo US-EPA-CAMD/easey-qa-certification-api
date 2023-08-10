4c09e0995039d06a3d61a42b40ea95db310d042a
Fahim Anwari <fahimanwari@cvpcorp.com>
2023-08-10 14:19:14 -0400

    Added @aws-sdk/client-s3 package
    Removed aws-sdk package
    
    Removed separate s3Client.js file. Client is now created in service
    
    Service and controller updated to make file upload work

--- src/s3-file-upload/s3-file-upload.controller.ts
+++ src/s3-file-upload/s3-file-upload.controller.ts
@@ -1,15 +1,30 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { S3FileUploadService } from './s3-file-upload.service';
 
 @Controller()
 @ApiTags("File Upload")
+@ApiSecurity('APIKey')
 export class S3FileUploadController {
 
+    constructor(private service: S3FileUploadService) {}
+
     @Post()
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+        schema: {
+          type: 'object',
+          properties: {
+            file: {
+              type: 'string',
+              format: 'binary',
+            },
+          },
+        },
+      })
     @UseInterceptors(FileInterceptor('file'))
-    @ApiSecurity('APIKey')
     uploadFile(@UploadedFile() file: Express.Multer.File) {
-        console.log(file);
+        this.service.uploadFile(file.originalname, file.buffer)
     }
 }
--- src/s3-file-upload/s3-file-upload.service.ts
+++ src/s3-file-upload/s3-file-upload.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 @Injectable()
-export class S3FileUploadService {}
+export class S3FileUploadService {
+
+    private s3Client;
+
+    constructor(private readonly configService: ConfigService){
+        if( !this.configService.get("AWS_S3_REGION") ||
+            !this.configService.get("AWS_S3_BUCKET_NAME") ||
+            !this.configService.get("AWS_ACCESS_KEY_ID") ||
+            !this.configService.get("AWS_SECRET_ACCESS_KEY")
+
+        ){
+            throw new Error("AWS information not set")
+        }
+
+        this.s3Client = new S3Client({
+            region: this.configService.get("AWS_S3_REGION")
+        })
+    }
+
+ 
+
+    async uploadFile(fileName: string, file: Buffer){
+        return await this.s3Client.send(new PutObjectCommand({
+            Body: file,
+            Key: fileName,
+            Bucket: this.configService.get("AWS_S3_BUCKET_NAME"),
+        }))
+    }
+}
