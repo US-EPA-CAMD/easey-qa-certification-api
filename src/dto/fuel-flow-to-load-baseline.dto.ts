88c1d2c6648166d5bf8d4b528d6d4915ac1fde3a
subrataroy1 <subrataroy@cvpcorp.com>
2023-09-02 23:27:23 -0400

    feat: add dto validations for fuel-flow-to-load-baseline

--- src/dto/fuel-flow-to-load-baseline.dto.ts
+++ src/dto/fuel-flow-to-load-baseline.dto.ts
@@ -1,42 +1,159 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { CheckCatalogService } from '@us-epa-camd/easey-common/check-catalog';
+import { IsInRange, IsValidCode } from '@us-epa-camd/easey-common/pipes';
+import {
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  ValidationArguments,
+} from 'class-validator';
+import { unitsOfMeasureCode } from '../entities/units-of-measure-code.entity';
+
 const KEY = 'Fuel Flow To Load Baseline';
 
 export class FuelFlowToLoadBaselineBaseDTO {
   @IsOptional()
-  @IsNumber()
-  accuracyTestNumber?: number;
+  @IsString()
+  @MaxLength(18, {
+    message: (args: ValidationArguments) => {
+      return `The value for [${args.value}] in the Component record [${args.property}] must not exceed 18 characters for [${KEY}].`;
+    },
+  })
+  accuracyTestNumber?: string;
+
   @IsOptional()
-  @IsNumber()
-  peiTestNumber?: number;
+  @IsString()
+  @MaxLength(18, {
+    message: (args: ValidationArguments) => {
+      return `The value for [${args.value}] in the Component record [${args.property}] must not exceed 18 characters for [${KEY}].`;
+    },
+  })
+  peiTestNumber?: string;
+
   @IsOptional()
-  @IsNumber()
+  @IsNumber(
+    { maxDecimalPlaces: 1 },
+    {
+      message: (args: ValidationArguments) => {
+        return `The value of [${args.value}] for [${args.property}] is allowed only 1 decimal place for $[${KEY}].`;
+      },
+    },
+  )
+  @IsInRange(0, 999999999.9, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 999999999.9 for [${KEY}].`;
+    },
+  })
   averageFuelFlowRate?: number;
+
   @IsOptional()
-  @IsNumber()
+  @IsInt()
+  @IsInRange(0, 999999, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 999999 for [${KEY}].`;
+    },
+  })
   averageLoad?: number;
+
   @IsOptional()
-  @IsNumber()
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    {
+      message: (args: ValidationArguments) => {
+        return `The value of [${args.value}] for [${args.property}] is allowed only 2 decimal place for $[${KEY}].`;
+      },
+    },
+  )
+  @IsInRange(0, 9999.99, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 9999.99 for [${KEY}].`;
+    },
+  })
   baselineFuelFlowToLoadRatio?: number;
+
   @IsOptional()
   @IsString()
+  @IsValidCode(unitsOfMeasureCode, {
+    message: (args: ValidationArguments) => {
+      return CheckCatalogService.formatMessage(
+        'You reported the value [value] for [fieldname], which is not in the list of valid values for [key].',
+        {
+          value: args.value,
+          fieldname: args.property,
+          key: KEY,
+        },
+      );
+    },
+  })
   fuelFlowToLoadUnitsOfMeasureCode?: string;
+
   @IsOptional()
-  @IsNumber()
+  @IsNumber(
+    { maxDecimalPlaces: 1 },
+    {
+      message: (args: ValidationArguments) => {
+        return `The value of [${args.value}] for [${args.property}] is allowed only 1 decimal place for $[${KEY}].`;
+      },
+    },
+  )
+  @IsInRange(0, 999999.9, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 999999.9 for [${KEY}].`;
+    },
+  })
   averageHourlyHeatInputRate?: number;
+
   @IsOptional()
-  @IsNumber()
+  @IsInt()
+  @IsInRange(0, 999999, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 999999 for [${KEY}].`;
+    },
+  })
   baselineGHR?: number;
+
   @IsOptional()
   @IsString()
+  @IsValidCode(unitsOfMeasureCode, {
+    message: (args: ValidationArguments) => {
+      return CheckCatalogService.formatMessage(
+        'You reported the value [value] for [fieldname], which is not in the list of valid values for [key].',
+        {
+          value: args.value,
+          fieldname: args.property,
+          key: KEY,
+        },
+      );
+    },
+  })
   ghrUnitsOfMeasureCode?: string;
+
   @IsOptional()
-  @IsNumber()
+  @IsInt()
+  @IsInRange(0, 9999, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 9999 for [${KEY}].`;
+    },
+  })
   numberOfHoursExcludedCofiring?: number;
+
   @IsOptional()
-  @IsNumber()
+  @IsInt()
+  @IsInRange(0, 9999, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 9999 for [${KEY}].`;
+    },
+  })
   numberOfHoursExcludedRamping?: number;
+
   @IsOptional()
-  @IsNumber()
+  @IsInt()
+  @IsInRange(0, 9999, {
+    message: (args: ValidationArguments) => {
+      return `The value of [${args.value}] for [${args.property}] must be within the range of 0 and 9999 for [${KEY}].`;
+    },
+  })
   numberOfHoursExcludedLowRange?: number;
 }
 
---
+++ src/entities/units-of-measure-code.entity.ts
@@ -0,0 +1,14 @@
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity({ name: 'camdecmpsmd.units_of_measure_code' })
+export class unitsOfMeasureCode extends BaseEntity {
+  @PrimaryColumn({
+    name: 'uom_cd',
+  })
+  unitsOfMeasureCode: string;
+
+  @Column({
+    name: 'uom_cd_description',
+  })
+  unitsOfMeasureCodeDescription: string;
+}
