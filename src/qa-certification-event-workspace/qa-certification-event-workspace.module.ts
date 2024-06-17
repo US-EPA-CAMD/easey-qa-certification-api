21724dc4a8818ae44774383996d40ecb570d7824
Yonatan Amezene <yamezene@decisionpointcorp.com>
2024-06-17 13:10:51 -0400

    QA certification changes

--- src/qa-certification-event-workspace/qa-certification-event-workspace.module.ts
+++ src/qa-certification-event-workspace/qa-certification-event-workspace.module.ts
@@ -2,11 +2,8 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ComponentWorkspaceRepository } from '../component-workspace/component.repository';
 import { ComponentModule } from '../component-workspace/component.module';
 import { QACertificationEventMap } from '../maps/qa-certification-event.map';
-import { MonitorLocationRepository } from '../monitor-location/monitor-location.repository';
-import { MonitorSystemWorkspaceRepository } from '../monitor-system-workspace/monitor-system-workspace.repository';
 import { MonitorLocationModule } from '../monitor-location/monitor-location.module';
 import { MonitorSystemWorkspaceModule } from '../monitor-system-workspace/monitor-system-workspace.module';
 import { QACertificationEventChecksService } from './qa-certification-event-checks.service';
@@ -14,6 +11,7 @@ import { QACertificationEventWorkspaceController } from './qa-certification-even
 import { QACertificationEventWorkspaceRepository } from './qa-certification-event-workspace.repository';
 import { QACertificationEventWorkspaceService } from './qa-certification-event-workspace.service';
 import { QACertificationEventRepository } from '../qa-certification-event/qa-certification-event.repository';
+import { QaCertificationEventModule } from '../qa-certification-event/qa-certification-event.module';
 
 @Module({
   imports: [
@@ -26,11 +24,8 @@ import { QACertificationEventRepository } from '../qa-certification-event/qa-cer
   ],
   controllers: [QACertificationEventWorkspaceController],
   providers: [
-    ComponentWorkspaceRepository,
-    MonitorLocationRepository,
-    MonitorSystemWorkspaceRepository,
     QACertificationEventWorkspaceRepository,
-    QACertificationEventRepository,
+    QaCertificationEventModule,
     QACertificationEventWorkspaceService,
     QACertificationEventMap,
     QACertificationEventChecksService,
