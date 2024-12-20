35f00fba64e953eebf478f1dd314f51d7f1b3e9b
ergjustin <justin.joseph.erg.com>
2024-12-20 13:34:48 -0600

    hg-summaries

--- src/hg-summary-workspace/hg-summary-workspace.controller.spec.ts
+++ src/hg-summary-workspace/hg-summary-workspace.controller.spec.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthGuard } from '@us-epa-camd/easey-common/guards';
 import { CurrentUser } from '@us-epa-camd/easey-common/interfaces';
+import { LoggerModule } from '@us-epa-camd/easey-common/logger';
 import { DataSource } from 'typeorm';
 
 import { HgSummaryBaseDTO, HgSummaryDTO } from '../dto/hg-summary.dto';
@@ -37,7 +38,7 @@ describe('HgSummaryWorkspaceController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
+      imports: [LoggerModule, HttpModule],
       controllers: [HgSummaryWorkspaceController],
       providers: [
         ConfigService,
