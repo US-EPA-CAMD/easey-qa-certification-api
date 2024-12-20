2a5187c148336e07d83d07d12704c8a884e548d8
ergjustin <justin.joseph.erg.com>
2024-12-20 10:48:30 -0600

    Flow To Load Check and testcase

--- src/flow-to-load-check-workspace/flow-to-load-check-workspace.controller.spec.ts
+++ src/flow-to-load-check-workspace/flow-to-load-check-workspace.controller.spec.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthGuard } from '@us-epa-camd/easey-common/guards';
 import { CurrentUser } from '@us-epa-camd/easey-common/interfaces';
+import { LoggerModule } from '@us-epa-camd/easey-common/logger';
 import { DataSource } from 'typeorm';
 
 import { FlowToLoadCheckBaseDTO } from '../dto/flow-to-load-check.dto';
@@ -38,7 +39,7 @@ describe('FlowToLoadCheckWorkspaceController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
+      imports: [LoggerModule, HttpModule],
       controllers: [FlowToLoadCheckWorkspaceController],
       providers: [
         ConfigService,
