8c05ea6da68e4ffc0c345038ad5d5084e3965a14
Jon Maxwell Diebold <max.diebold@erg.com>
2025-03-04 19:20:48 -0500

    test(6110): fixed failing unit tests

--- src/mats-data-submission/mats-data-submission.controller.spec.ts
+++ src/mats-data-submission/mats-data-submission.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { MatsCodeMap } from '../maps/mats-code.map';
 import { MatsDataSubmissionController } from './mats-data-submission.controller';
+import { MatsDataSubmissionMap } from '../maps/mats-data-submission.map';
+import { MatsDataSubmissionService } from './mats-data-submission.service';
 
 describe('MatsDataSubmissionController', () => {
   let controller: MatsDataSubmissionController;
@@ -7,9 +11,16 @@ describe('MatsDataSubmissionController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MatsDataSubmissionController],
+      providers: [
+        MatsCodeMap,
+        MatsDataSubmissionMap,
+        MatsDataSubmissionService,
+      ],
     }).compile();
 
-    controller = module.get<MatsDataSubmissionController>(MatsDataSubmissionController);
+    controller = module.get<MatsDataSubmissionController>(
+      MatsDataSubmissionController,
+    );
   });
 
   it('should be defined', () => {
--- src/mats-data-submission/mats-data-submission.service.spec.ts
+++ src/mats-data-submission/mats-data-submission.service.spec.ts
@@ -1,4 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { EntityManager } from 'typeorm';
+
+import { MatsCodeMap } from '../maps/mats-code.map';
+import { MatsDataSubmissionMap } from '../maps/mats-data-submission.map';
+import { MatsDataSubmissionRepository } from './mats-data-submission.repository';
 import { MatsDataSubmissionService } from './mats-data-submission.service';
 
 describe('MatsDataSubmissionService', () => {
@@ -6,7 +11,13 @@ describe('MatsDataSubmissionService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MatsDataSubmissionService],
+      providers: [
+        EntityManager,
+        MatsCodeMap,
+        MatsDataSubmissionMap,
+        MatsDataSubmissionRepository,
+        MatsDataSubmissionService,
+      ],
     }).compile();
 
     service = module.get<MatsDataSubmissionService>(MatsDataSubmissionService);
