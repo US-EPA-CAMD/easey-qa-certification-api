be0a1565c2e8d441e24a287e89831631b9ef58e0
Jon Maxwell Diebold <max.diebold@erg.com>
2025-03-06 11:59:11 -0500

    test(6110): one more failing unit test

--- src/mats-data-submission/mats-data-submission.controller.spec.ts
+++ src/mats-data-submission/mats-data-submission.controller.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { EntityManager } from 'typeorm';
 
 import { MatsCodeMap } from '../maps/mats-code.map';
 import { MatsDataSubmissionController } from './mats-data-submission.controller';
 import { MatsDataSubmissionMap } from '../maps/mats-data-submission.map';
+import { MatsDataSubmissionRepository } from './mats-data-submission.repository';
 import { MatsDataSubmissionService } from './mats-data-submission.service';
 
 describe('MatsDataSubmissionController', () => {
@@ -12,8 +14,10 @@ describe('MatsDataSubmissionController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MatsDataSubmissionController],
       providers: [
+        EntityManager,
         MatsCodeMap,
         MatsDataSubmissionMap,
+        MatsDataSubmissionRepository,
         MatsDataSubmissionService,
       ],
     }).compile();
