73c8886ab213e6e4ea95e6f6e07a4f243e564179
Yonatan Amezene <yamezene@decisionpointcorp.com>
2025-05-30 08:48:15 -0400

    updated common version with GatewayGuard functionality to skip /health endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -190,6 +190,12 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(MaintenanceMiddleware)
+      .exclude(
+        {
+          path: '/health',
+          method: RequestMethod.GET,
+        },
+      )
       .forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }
