46f852e9a01a0c48c2fa4e8074e9d675b6613013
ergjustin <justin.joseph.erg.com>
2026-01-13 09:04:35 -0600

    update the testcase

--- src/mats-data-submission/mats-data-submission.service.spec.ts
+++ src/mats-data-submission/mats-data-submission.service.spec.ts
@@ -12,6 +12,7 @@ import { Plant } from '../entities/plant.entity';
 import { MatsDataSubmissionMap } from '../maps/mats-data-submission.map';
 import { MatsDataSubmissionRepository } from './mats-data-submission.repository';
 import { MatsDataSubmissionService } from './mats-data-submission.service';
+import { MatsReportTypeCode } from '../entities/mats-report-type-code.entity';
 
 const mockDate = new Date();
 function createMockSubmission() {
@@ -20,6 +21,9 @@ function createMockSubmission() {
   pollutant.metadataPollutantCode = 'ANY';
   const testMethod = new MatsTestMethodCode();
   testMethod.code = 'MTH';
+
+  const reportType = new MatsReportTypeCode();
+  reportType.metadataReportTypeCode = 'RPT'; 
 
   entity.addTime = mockDate;
   entity.averagingGroupCode = 'AGC';
@@ -34,6 +38,7 @@ function createMockSubmission() {
   entity.pollutants = [pollutant];
   entity.quarter = 1;
   entity.reportTypeCode = 'RPT';
+  entity.reportType = reportType;
   entity.testComment = 'Test Comment';
   entity.testDate = mockDate;
   entity.testMethods = [testMethod];
